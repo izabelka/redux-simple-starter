37435b43636409eb0bad370e51eacbee51835545
izabelka <i.konstanciak@gmail.com>
2017-02-17 21:01:58 +0100

    Code refactoring - index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyBKBu77HCUT6I3oZiFXlaIdou8S_3voo5E';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
